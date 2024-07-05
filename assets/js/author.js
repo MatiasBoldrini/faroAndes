ead9051734343b44bc6f206e9a431472bd0a41f3
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2024-07-05 14:24:26 -0300

    ok

--- assets/js/author.js
+++ assets/js/author.js
@@ -7,7 +7,7 @@ const talentos = {
     DanielPortnoy: {
         name: 'Daniel Portnoy',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Daniel-Portnoy.jpg',
         linkedin: 'https://www.linkedin.com/in/daniel-portnoy-8884471/',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -15,7 +15,6 @@ const talentos = {
     LeonardoAmurrio: {
         name: 'Leonardo Amurrio',
         areas: [],
-        position: 'En desarrollo',
         photo: 'assets/extra/Talentos/Leonardo-Amurrio.jpg',
         linkedin: '#',
         description: '<p class="text-lg mb-0">Me considero un incansable buscador de la <strong>verdad</strong>. Desde muy temprana edad he experimentado una inmensa pasión por los <strong>misterios</strong> ocultos de la vida. Formar parte de este revolucionario equipo de investigación y excelente grupo humano brinda a mi vida una gran cuota de plenitud y realización.</p><p class="text-lg mb-0">    Soy Leonardo Demian Amurrio, un profesional argentino con formación académica en Ingeniería Electrónica y Licenciatura en Física, principalmente enfocado en la investigación de tecnologías de nuevas energías y diversos campos de la <strong>ciencia</strong> disruptiva y no convencional.</p><p class="text-lg mb-0">    <strong>Autodidacta</strong> y multifacético. He dedicado gran parte de mi vida a adquirir todo tipo de habilidades y conocimientos que me ayuden a formar parte activa de la co-creación de un mundo más próspero. Desde el ámbito técnico y electrónico, como la programación de bajo y alto nivel orientada a redes neuronales, sistemas embebidos y microcontroladores, hasta la bioconstrucción, agroecología, permacultura y el estudio de diversas filosofías ontológicas y sistemas revolucionarios de organización social.</p><p class="text-lg mb-0">    Creo firmemente que un nuevo mundo es posible, solo está en nosotros hacerlo <strong>realidad</strong>.</p>'
@@ -23,7 +22,6 @@ const talentos = {
     MauricioMiranda: {
         name: 'Mauricio Miranda',
         areas: [],
-        position: 'Ingeniero de Software',
         photo: 'assets/extra/Talentos/Mauricio-Miranda.jpeg',
         linkedin: '',
         description: '<p class="text-lg mb-0"> Mauricio soluciona problemas escribiendo <strong>código</strong>. Es ingeniero de software con amplia experiencia en diseño y arquitectura de aplicaciones, liderazgo de equipos y gestión de proyectos. Cuenta con más de 20 años de experiencia en desarrollo de software en múltiples industrias, con un fuerte enfoque en tecnologías <strong>geoespaciales</strong> </p> <p class="text-lg mb-0"> Mauricio se enamoró del proyecto apenas escuchó hablar del Faro. Su intención es colaborar con el equipo aportando su <strong>conocimiento</strong> en el desarrollo de software y su experiencia en la búsqueda de soluciones con una perspectiva pragmática e <strong>iterativa</strong>.</p>'
@@ -31,7 +29,6 @@ const talentos = {
     MarceloBoldrini: {
         name: 'Marcelo Boldrini',
         areas: [],
-        position: 'position',
         photo: 'assets/extra/Talentos/Marcelo-Boldrini.jpeg',
         linkedin: 'https://www.linkedin.com/in/marmatbol/',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -39,7 +36,7 @@ const talentos = {
     SebastianSpalenza: {
         name: 'Sebastian Spalenza',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Sebastian-Spalenza.jpg',
         linkedin: 'https://www.linkedin.com/in/sebastian-spalenza-77507155/',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -47,15 +44,15 @@ const talentos = {
     PedroMiralles: {
         name: 'Pedro Miralles',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Pedro-Miralles.jpg',
         linkedin: 'https://www.linkedin.com/in/pedro-miralles-070730174/',
         description: '<p class="text-lg mb-0"> Description</p>'
-    }, 
+    },
     MarianoChicatun: {
         name: 'Mariano Chicatún',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Foto-Mariano-Chicatun.jpg',
         linkedin: 'https://www.linkedin.com/in/mariano-chicatun/',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -63,7 +60,7 @@ const talentos = {
     RobertoAquilano: {
         name: 'Roberto Aquilano',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Roberto-Aquilano.jpeg',
         linkedin: 'https://www.linkedin.com/in/roberto-oscar-aquilano-a0bba745/',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -71,7 +68,7 @@ const talentos = {
     VictoriaColombo: {
         name: 'Victoria Colombo',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Foto-Clara-Victoria-Colombo.jpg',
         linkedin: 'https://www.linkedin.com/in/victoria-colombo-531ab71b2',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -79,7 +76,7 @@ const talentos = {
     LeonardoGuse: {
         name: 'Leonardo Guse',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Leonardo-Guse.jpeg',
         linkedin: '',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -87,7 +84,7 @@ const talentos = {
     CoquiPace: {
         name: 'Coqui Pace',
         areas: [],
-        position: 'position',
+
         photo: 'assets/extra/Talentos/Coqui-pace.png',
         linkedin: '',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -95,15 +92,14 @@ const talentos = {
     RaulComelli: {
         name: 'Raul Comelli',
         areas: [],
-        position: 'position',
-        photo: 'assets/extra/Talentos/Raúl-Comelli.jpg',
+
+        photo: 'assets/extra/Talentos/Raul-Comelli.jpg',
         linkedin: '',
         description: '<p class="text-lg mb-0"> Description</p>'
     },
     ManuelaPerez: {
         name: 'Manuela Perez',
         areas: [],
-        position: 'position',
         photo: 'assets/extra/Talentos/Foto-Manuela-Perez.jpg',
         linkedin: '',
         description: '<p class="text-lg mb-0"> Description</p>'
@@ -114,49 +110,71 @@ const areas_investigacion = {
         talentos_investigacion: [
             talentos['LeonardoGuse'],
             talentos['RaulComelli'],
-        ]
+        ],
+        pageName: 'catalisis',
     },
     'AI aplicada a I+D': {
         talentos_investigacion: [
             talentos['MarianoChicatun'],
-        ]
+        ],
+        pageName: 'AI-Inv-Desarrollo',
     },
     'Soluciones ingeniería aeroespacial': {
         talentos_investigacion: [
             talentos['DanielPortnoy'],
-        ]
+        ],
+        pageName: 'ingenieria-espacial',
     },
     'Tecnologías Alternativas para la Generación de Hidrógeno': {
         talentos_investigacion: [
             talentos['SebastianSpalenza'],
             talentos['PedroMiralles'],
             talentos['LeonardoAmurrio'],
-            talentos['LeonardoAmurrio'],
-        ]
+        ],
+        pageName: 'energias-alternativas-hidrogeno',
     },
     'Tecnología de Nuevas Energías': {
         talentos_investigacion: [
             talentos['PedroMiralles'],
             talentos['LeonardoAmurrio']
-        ]
+        ],
+        pageName: 'tecnologia-nuevas-energias',
     },
     'Lanzadores Espaciales': {
         talentos_investigacion: [
             talentos['CoquiPace']
-        ]
+        ],
+        pageName: 'lanzadores-espaciales',
     },
     'Viviendas Sustentables': {
         talentos_investigacion: [
             // talentos['ClaraColombo'],
             talentos['ManuelaPerez'],
             talentos['SebastianSpalenza']
-        ]
+        ],
+        pageName: 'viviendas-sustentables',
     }
 };
 function showEmployeeDetail() {
     const talentoKey = getQueryParam('talento');
     const talento = talentos[talentoKey];
-    console.log(talento);
+    for (let area in areas_investigacion) {
+        if (area != 'pageName') {
+            const empleados = areas_investigacion[area].talentos_investigacion;
+            empleados.forEach(empleado => {
+                console.log(empleado);
+                // Verificar si el empleado existe y tiene la propiedad 'areas'
+                if (empleado.name) {
+                    console.log(empleado + ' Asignado a ' + area.talentos_investigacion);
+                    // Agregar el área al empleado
+                    empleado.areas.push(area);
+                } else {
+                    console.error(`Empleado inválido o sin propiedades definidas: ${empleado}`);
+                }
+            });
+        }
+
+    }
     if (talento) {
         const detailContainer = document.getElementById('talento-detail-container');
         const imageContainer = document.getElementById('author-image-container');
@@ -172,39 +190,27 @@ function showEmployeeDetail() {
                 </div>
                 <div class="row mb-4">
                   <div class="col-auto">
-                    <span>${talento.position}</span>
-                  </div>
-                  <div class="col-auto">
-                    <span>${talento.areas}</span>
-                  </div>
-                  <div class="col-auto">
-                    
+                    ${Object.keys(areas_investigacion).map(area => {
+                        // Verificar si el área está asignada al empleado
+                        if (areas_investigacion[area].talentos_investigacion.includes(talento)) {
+                            return `
+                                <a href="${areas_investigacion[area].pageName}.html">
+                                    <span>${area}</span>
+                                </a>
+                            `;
+                        }
+                        return ''; // Si el área no está asignada, retorna una cadena vacía
+                    }).join('')}
+                </div>
 
-                  </div>
                 </div>
                 ${talento.description}
             `;
     }
 }
-function asignarAreasAEmpleados() {
-    console.log(Object.keys(talentos).length);
-    // Iterar sobre cada área de investigación
-    for (let area in areas_investigacion) {
-        const empleados = areas_investigacion[area].talentos_investigacion;
-        console.log(empleados);
-        empleados.forEach(empleado => {
-            // Verificar si el empleado existe y tiene la propiedad 'areas'
-            if (empleado && empleado.areas) {
-                // Agregar el área al empleado
-                empleado.areas.push(area);
-            } else {
-                console.error(`Empleado inválido o sin propiedades definidas: ${empleado}`);
-            }
-        });
-    }
-
+function showEmployeeAreas() {
+    
 }
 
-asignarAreasAEmpleados();
 
 showEmployeeDetail();
